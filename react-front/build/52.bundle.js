0d33f851973827dd9512de16106cbceef9a5e18e
mkkekkonen2 <mkkekkonen@protonmail.com>
2020-03-27 22:08:07 +0200

    Build

---
+++ react-front/build/52.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{823:function(n,e){n.exports=function(n){return{subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+//# sourceMappingURL=52.bundle.js.map
