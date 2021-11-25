6d04a5176f1b42649f829cf32d1cdaea66b8460f
mkkekkonen2 <mkkekkonen@protonmail.com>
2021-11-25 22:28:19 +0200

    Upgrade packages in API, admin and React projects + change math project name to mk-ts-math, use in React project + fix Immutable types in React project + remove yarn.lock files

--- react-front/src/store/sagas/categories.ts
+++ react-front/src/store/sagas/categories.ts
@@ -8,10 +8,10 @@ import { Category, ICategory } from '../../models';
 
 function* loadCategories() {
   try {
-    const categoryData = yield categoryApi.getAllCategories();
+    const categoryData: ICategory[] = yield categoryApi.getAllCategories();
     const categoryArr = categoryData.map((dataItem: ICategory) =>
       new Category(dataItem.id, dataItem.name, dataItem.index));
-    const categoryList = Immutable.fromJS(categoryArr);
+    const categoryList = Immutable.List(categoryArr);
     yield put(mergeCategories(categoryList));
   } catch(e) {
     console.error(`Error fetching categories: ${e.message}`);
