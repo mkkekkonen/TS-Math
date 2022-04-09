7e1fa5c524ace4dcfe9a821956dec968dfc73f14
mkkekkonen2 <mkkekkonen@protonmail.com>
2022-04-09 22:53:59 +0300

    Change mkkekkonen.com to mkkekkonen.fi in admin and React frontend

--- admin/src/services/api/settings.ts
+++ admin/src/services/api/settings.ts
@@ -13,7 +13,7 @@ export const getApiBaseUrl = () => {
 
   switch (process.env.NODE_ENV) {
     case 'production':
-      return 'https://www.mkkekkonen.com/math-api';
+      return 'https://www.mkkekkonen.fi/math-api';
     case 'development':
     default:
       return process.env.MOBILE ? 'http://192.168.1.66:3100' : 'http://localhost:3100';
